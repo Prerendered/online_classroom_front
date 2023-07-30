94be09918f8794933f36d8c807df6085a0fb218d
Prerendered <20709154@student.curtin.edu.au>
2023-07-30 19:35:08 +0400

    added student + teacher dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ViewVideos from './pages/viewvideos.js';
 import LoginPage from './pages/loginpage.js';
 import UploadVideos from './pages/uploadvids.js';
 import UploadMats from './pages/uploadmats.js';
+import TeacherPage from './pages/teacher_dashboard.js';
+import StudentPage from './pages/student_dashboard.js';
 import './App.css';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       {/* <UploadMats /> */}
       {/* <UploadVideos />   */} {/* error in this shit */}
       <LoginPage/>
+      {/* <TeacherPage/> */}
+      {/* <StudentPage/> */}
     </div>
   );
 }
