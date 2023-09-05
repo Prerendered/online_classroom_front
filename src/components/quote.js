dcaab9191d3dc5887342b8e07280dd7b9fe6a7f6
Prerendered <20709154@student.curtin.edu.au>
2023-09-05 22:04:41 +0400

    Changes: added automatic quotes from API

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Paper } from '@mui/material';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-//Quote section where a quote of the day will be generated
+import axios from 'axios';
+
 const QuoteOfTheDay = () => {
+    const category = "inspirational";
+
+    const [quoteData, setQuote] = useState(null);
+
+    const quoteAPI = () => {
+        axios.get(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+            headers: { 'X-Api-Key': 'O6mG+R9hg1Sh85AX7rieHw==rLrMmujHyDQm9hWA' }
+        })
+        .then((response) => {
+            // Assuming response.data contains the quote
+            console.log(response.data);
+            setQuote(response.data[0]);
+        })
+        .catch((error) => {
+            console.error('Error', error);
+            console.log("Error Request: ", error.request);
+            console.log("Error Response: ", error.response);
+        });
+    };
+
+    useEffect(() => {
+        quoteAPI();
+    }, []);
 
     return (
         <Box marginY={5}>
-        <Paper elevation={4}>
-            <Box paddingX={1} paddingBottom={25}>
-            <Typography variant="h4" component="h3" align='left' fontWeight="bold">Quote of the Day</Typography>
-            </Box>
-          </Paper>
-          </Box>
-    )
-}
+            <Paper elevation={4}>
+                <Box padding={1}>
+                    <Typography variant="h4" component="h3" align='left' fontWeight="bold">
+                        Quote of the Day
+                    </Typography>
+                    {quoteData && (
+                        <Box>
+                            <Typography variant="body1" component="p" align='center' padding={5}>
+                                {quoteData.quote}  
+                            </Typography>
+
+                            <Typography variant="body2" component="p" align='right'>
+                            - {quoteData.author}
+                            </Typography>
+                        </Box>
+                    )}
+                </Box>
+            </Paper>
+        </Box>
+    );
+};
+
 export default QuoteOfTheDay;
