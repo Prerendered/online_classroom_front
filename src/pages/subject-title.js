522c54c923ac2b6e42788aa20ae058465997ea1f
Prerendered <20709154@student.curtin.edu.au>
2023-09-08 00:12:10 +0400

    Changes: taking title of video from url

--- src/pages/subject-title.js
+++ src/pages/subject-title.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Typography } from '@mui/material';
+import { useParams } from 'react-router-dom'; // Import useParams to get name from url
 
 const Subtitle = () => {
+  const { name } = useParams(); // Get the name from URL
+
   return (
-    <div className="subject-title">
-        <h1 className='unit'>Math</h1>
-        <h2 className='subchapter'>Chapter 1 : Linear Algebra</h2>
-    </div>
+    <Typography variant="h2" component="h2" align="left" style={{ marginLeft: '22%', marginTop: '1%', }}>
+      {name}  {/* name taken from url*/}
+    </Typography>
   );
 };
 
