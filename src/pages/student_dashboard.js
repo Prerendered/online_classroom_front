3d27e36836ce630eba9142b3a611eb2d5d6179d2
Prerendered <20709154@student.curtin.edu.au>
2023-10-17 21:04:13 +0400

    Changes: added welcome view to dashboards

--- src/pages/student_dashboard.js
+++ src/pages/student_dashboard.js
@@ -1,11 +1,11 @@
-import "./student_dashboard.css";
+import React, { useRef } from "react"; // Import useRef
 import Navbar from "../components/navbar.js";
 import SubjectsTable from "../components/subjectTable.js";
-// import BasicDateCalendar from '../components/calendar.js';
 import StudentCalendar from "../components/studentCalendar.js";
 import QuoteOfTheDay from "../components/quote";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button"; // Import Button
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
@@ -14,32 +14,74 @@ const theme = createTheme({
   },
 });
 
-//Welcome message
-function Welcome() {
+function FullScreenWelcome({ onButtonClick }) {
   return (
-    <Grid container justify="flex-start" padding={5}>
-      <Typography variant="h2" component="h1">
-        Welcome to your dashboard!
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1491841550275-ad7854e35ca6?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1974")', // Set the background image here
+        backgroundSize: "cover", // Ensure the image covers the entire div
+        backgroundPosition: "center", // Center the background image
+      }}
+    >
+      <Typography variant="h3" component="h1" sx={{ color: "#FFFFFF" }}>
+        Welcome
       </Typography>
-    </Grid>
+      <Typography variant="h1" component="h1" sx={{ color: "#FFFFFF" }}>
+        Online Classroom
+      </Typography>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={onButtonClick}
+        style={{
+          marginTop: "20px",
+          borderRadius: "0",
+          color: "#FFFFFF",
+          borderColor: "#FFFFFF",
+        }}
+      >
+        Go to your courses
+      </Button>
+    </div>
   );
 }
 
 function studentdashboard() {
+  const dashboardRef = useRef(null); // Create a reference
+
+  const handleButtonClick = () => {
+    dashboardRef.current.scrollIntoView({ behavior: "smooth" }); // Scroll smoothly to the reference
+  };
+
   return (
     <ThemeProvider theme={theme}>
+      <Navbar />
       <div className="App">
-        <Navbar />
-        <Welcome />
-        <Grid container spacing={10} padding={5}>
-          <Grid item xs={7}>
-            <SubjectsTable />
+        <FullScreenWelcome onButtonClick={handleButtonClick} />
+        <div ref={dashboardRef}>
+          {" "}
+          {/* Set the reference here */}
+          <Grid
+            container
+            spacing={10}
+            padding={5}
+            style={{ marginTop: "5%", marginBottom: "12%" }}
+          >
+            <Grid item xs={7}>
+              <SubjectsTable />
+            </Grid>
+            <Grid item xs={4}>
+              <StudentCalendar />
+              <QuoteOfTheDay />
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <StudentCalendar />
-            <QuoteOfTheDay />
-          </Grid>
-        </Grid>
+        </div>
       </div>
     </ThemeProvider>
   );
--- src/pages/teacher_dashboard.js
+++ src/pages/teacher_dashboard.js
@@ -1,4 +1,4 @@
-import "./teacher_dashboard.css";
+import React, { useRef } from "react"; // Import useRef
 import Navbar from "../components/navbar.js";
 import SubjectsTable from "../components/teacherSubjectTable.js";
 import TeacherCalendar from "../components/teacherCalendar";
@@ -6,6 +6,7 @@ import QuoteOfTheDay from "../components/quote";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button"; // Import Button
 
 const theme = createTheme({
   typography: {
@@ -13,32 +14,74 @@ const theme = createTheme({
   },
 });
 
-//Welcome message
-function Welcome() {
+function FullScreenWelcome({ onButtonClick }) {
   return (
-    <Grid container justify="flex-start" padding={5}>
-      <Typography variant="h2" component="h1">
-        Welcome to your dashboard!
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1533709475520-a0745bba78bf?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=2070")', // Set the background image here
+        backgroundSize: "cover", // Ensure the image covers the entire div
+        backgroundPosition: "center", // Center the background image
+      }}
+    >
+      <Typography variant="h3" component="h1" sx={{ color: "#FFFFFF" }}>
+        Welcome
       </Typography>
-    </Grid>
+      <Typography variant="h1" component="h1" sx={{ color: "#FFFFFF" }}>
+        Online Classroom
+      </Typography>
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={onButtonClick}
+        style={{
+          marginTop: "20px",
+          borderRadius: "0",
+          color: "#FFFFFF",
+          borderColor: "#FFFFFF",
+        }}
+      >
+        Go to your courses
+      </Button>
+    </div>
   );
 }
 
 function teacherdashboard() {
+  const dashboardRef = useRef(null); // Create a reference
+
+  const handleButtonClick = () => {
+    dashboardRef.current.scrollIntoView({ behavior: "smooth" }); // Scroll smoothly to the reference
+  };
+
   return (
     <ThemeProvider theme={theme}>
+      <Navbar />
       <div className="App">
-        <Navbar />
-        <Welcome />
-        <Grid container spacing={10} padding={5}>
-          <Grid item xs={7}>
-            <SubjectsTable />
+        <FullScreenWelcome onButtonClick={handleButtonClick} />
+        <div ref={dashboardRef}>
+          {" "}
+          {/* Set the reference here */}
+          <Grid
+            container
+            spacing={10}
+            padding={5}
+            style={{ marginTop: "5%", marginBottom: "12%" }}
+          >
+            <Grid item xs={7}>
+              <SubjectsTable />
+            </Grid>
+            <Grid item xs={4}>
+              <TeacherCalendar />
+              <QuoteOfTheDay />
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <TeacherCalendar />
-            <QuoteOfTheDay />
-          </Grid>
-        </Grid>
+        </div>
       </div>
     </ThemeProvider>
   );
